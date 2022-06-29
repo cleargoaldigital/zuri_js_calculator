bade73038ffcc77fb2f4382f341331283465b5d2
taofeek ajibade <cleargoal.ng@gmail.com>
2022-06-29 18:40:48 +0100

    added comments in code.

--- calc.js
+++ calc.js
@@ -1,28 +1,26 @@
-let firstNum = prompt("Enter a digit");
-let secondNum = prompt("Enter second digit");
+let firt_number = parseInt(prompt("Enter a digit"));
+let second_number = parseInt(prompt("Enter second digit"));
 
-let choice = prompt('Enter an operator, + = addition, - = subtraction, / = division, * = multiplication and % = remainder: ')
+// selection of choice of operator
+let choice = prompt('Enter an operator, + = addition, - = subtraction, / = division, * = multiplication ')
+
+// the sum of the two numbers variable
+let add = firt_number + second_number;
+let subtract = firt_number - second_number;
+let divide = firt_number / second_number;
+let multiply = firt_number * second_number;
+
 //result output based on selected operators
 
-if(choice = "+"){
-   return firstNum + secondNum;
+if(choice === "+"){
+   alert ('The sum of ' + firt_number + ' and ' + second_number + ' is: ' + add);
 }
-
-else if (choice = "-"){
-   return firstNum - secondNum;
+else if (choice === "-"){
+   alert('The result of ' + firt_number + ' minus' + second_number + ' is: ' + subtract);
 }
-
-else if (choice = "*"){
-   return firstNum * secondNum;
+else if (choice === "/"){
+   alert('The result of ' + firt_number + ' divided by' + second_number + ' is: ' + divide);
 }
-
-else if (choice = "/"){
-   return firstNum / secondNum;
-}
-
-// let person = prompt("Please enter your name", "Harry Potter");
-
-if (person != null) {
-  document.getElementById("demo").innerHTML =
-  "Hello " + person + "! How are you today?";
+else if (choice === "*"){
+   alert('The result of ' + firt_number + ' multiplied by' + second_number + ' is: ' + multiply);
 }
